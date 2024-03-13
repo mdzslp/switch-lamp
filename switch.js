64715c3a09442efdc29d5c24eaf6ee158200d05f
mdzslp <150118879+mdzslp@users.noreply.github.com>
2024-03-13 13:16:37 +0300

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,27 +1,33 @@
-lamp = [{color: "red"}, {color: "yellow"}, {color: "green"}];
+let lamp = [{color: "red"}, {color: "yellow"}, {color: "green"}, {color: "blue"}];
+    let colorIndex = 0;
 
-let colorIndex = 1;
+    let colorsContainer = document.querySelector('.back_color');
 
-const colors = document.querySelectorAll('.back_color div');
+    for (let i = 0; i < lamp.length; i++) {
+        let colorDiv = document.createElement('div');
+        colorDiv.style.backgroundColor = lamp[i].color;
+        colorsContainer.appendChild(colorDiv);
+    }
 
-function switchColor() {
-    colors[colorIndex].style.backgroundColor = lamp[colorIndex].color;
-    for (let i = 0; i < colors.length; i++) {
-        if (i !== colorIndex) {
-            colors[i].style.backgroundColor = 'gray';
+    let colors = document.querySelectorAll('.back_color div');
+
+    function switchColor() {
+        colors[colorIndex].style.backgroundColor = lamp[colorIndex].color;
+        for (let i = 0; i < colors.length; i++) {
+            if (i !== colorIndex) {
+                colors[i].style.backgroundColor = 'gray';
+            }
         }
     }
-}
 
-switchColor();
-
-document.querySelector('.minus').addEventListener('click', function() {
-    colorIndex = (colorIndex + 1);
     switchColor();
-});
 
-document.querySelector('.plus').addEventListener('click', function() {
-    colorIndex = (colorIndex - 1 ) ;
-    switchColor();
-});
+    document.querySelector('.minus').addEventListener('click', function() {
+        colorIndex = (colorIndex + 1) % lamp.length;
+        switchColor();
+    });
 
+    document.querySelector('.plus').addEventListener('click', function() {
+        colorIndex = (colorIndex - 1 + lamp.length) % lamp.length;
+        switchColor();
+    });
